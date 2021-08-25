987bafd97dec04dab0d92163b654325e289c4734
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-08-25 19:49:02 -0300

    move: actions & reducers to redux folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 
-import { rootReducer } from "./reducers";
+import { store } from "./redux/store";
 import "./index.css";
 import App from "./app/App";
-
-const middleware = [thunk];
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 ReactDOM.render(
   <React.StrictMode>
